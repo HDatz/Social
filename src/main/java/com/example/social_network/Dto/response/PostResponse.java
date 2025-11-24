48fd8f6ac27772583d9357011b6aec9a284ed97b
HDatz <93897593+HDatz@users.noreply.github.com>
2025-11-25 00:34:58 +0700

    change expression Mapper

--- src/main/java/com/example/social_network/Dto/response/PostResponse.java
+++ src/main/java/com/example/social_network/Dto/response/PostResponse.java
@@ -23,4 +23,6 @@ public class PostResponse {
 
     String userId;
     String fullName;
+
+    int totalComment;
 }
--- src/main/java/com/example/social_network/Repository/CommentRepository.java
+++ src/main/java/com/example/social_network/Repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.example.social_network.Repository;
 
 import com.example.social_network.Entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, String> {
    List<Comment> findByPost_PostIdAndParentCommentIsNullOrderByTimeCommentAsc(String postId);
+
+   int countByPost_PostId(String postId);
+
 }
