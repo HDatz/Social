2443d6c40f8a7ff15db569c8d1132b9ef7ffb9f5
HDatz <93897593+HDatz@users.noreply.github.com>
2025-11-24 17:55:22 +0700

    add expression

--- src/main/java/com/example/social_network/Entity/Expression.java
+++ src/main/java/com/example/social_network/Entity/Expression.java
@@ -14,20 +14,43 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "expression",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"user_id", "post_id"}),
+            @UniqueConstraint(columnNames = {"user_id", "comment_id"})
+        },indexes = {
+            @Index(columnList = "post_id"),
+            @Index(columnList = "comment_id"),
+            @Index(columnList = "expressionName")
+        })
 public class Expression {
     @Id
-    String experssionId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String expressionId;
+
+    @Column(nullable = false)
+    String expressionName;
+
+    String description;
+    String icon;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     User user;
 
     @ManyToOne
+    @JoinColumn(name = "post_id")
     Post post;
 
     @ManyToOne
+    @JoinColumn(name = "comment_id")
     Comment comment;
 
-    LocalDateTime timeExperssion;
+    LocalDateTime timeExpression;
+
+    @PrePersist
+    protected  void onCreate(){
+        timeExpression = LocalDateTime.now();
+    }
 
 }
