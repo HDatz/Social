12aab6ed0825bc57713714fa4388f8628059c549
HDatz <93897593+HDatz@users.noreply.github.com>
2025-11-24 17:55:45 +0700

    add expression

---
+++ src/main/java/com/example/social_network/Entity/ExpressionType.java
@@ -0,0 +1,20 @@
+package com.example.social_network.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExpressionType {
+    @Id
+    String expressionName;
+
+    String descriptionExpression;
+}
